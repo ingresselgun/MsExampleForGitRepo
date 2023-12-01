024e5929f6916c0ecc7840d74d39aa707f519471
ingresselgun <ingresselgun010@gmail.com>
2023-12-01 19:27:23 +0400

    bezi buglari fix elemisem

--- Ant.java
+++ Ant.java
@@ -1,6 +1,7 @@
 public class Ant {
     public static void main(String[] args) {
         System.out.println("Salam");
+        //asdasdas
     }
 }
 
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(" Hello from Microservices ");
-
+        System.out.println("saagol elgun gite yukledim");
     }
 }
 //working directory-qirmizi
